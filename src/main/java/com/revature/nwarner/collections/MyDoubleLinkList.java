db02417422b571a04a85f999ed29a0b87b1cfd38
nwarner31 <natedawg3124@gmail.com>
2022-02-08 20:38:51 -0800

    Added MyDoubleLinkList class and tested current functionality

--- src/main/java/com/revature/nwarner/collections/MyDoubleLinkList.java
+++ src/main/java/com/revature/nwarner/collections/MyDoubleLinkList.java
@@ -1,4 +1,69 @@
 package com.revature.nwarner.collections;
 
+
+
 public class MyDoubleLinkList {
+
+    private MyDoubleLinkListNode head;
+    private MyDoubleLinkListNode tail;
+
+    private class MyDoubleLinkListNode {
+        private Object myValue;
+        private MyDoubleLinkListNode previousNode;
+        private MyDoubleLinkListNode nextNode;
+
+        private MyDoubleLinkListNode(Object value, MyDoubleLinkListNode previous, MyDoubleLinkListNode next) {
+            this.myValue = value;
+            this.previousNode = previous;
+            this.nextNode = next;
+        }
+    }
+
+    /**
+     * Inserts an object at the front of the linked list.
+     * @param o The object to be inserted.
+     */
+    public void push(Object o) {
+        MyDoubleLinkListNode node = new MyDoubleLinkListNode(o, null, head);
+        if (head != null) {
+            head.previousNode = node;
+        }
+        head = node;
+        if (tail == null) {
+            tail = node;
+        }
+
+    }
+
+    /**
+     * Inserts an object at the back of the linked list.
+     * @param o The object to be inserted.
+     */
+    public void add(Object o) {
+        MyDoubleLinkListNode node = new MyDoubleLinkListNode(o, tail, null);
+        if (tail != null) {
+            tail.nextNode = node;
+        }
+
+        tail = node;
+        if (head == null) {
+            head = node;
+        }
+    }
+
+    public void printForward() {
+        MyDoubleLinkListNode currentNode = head;
+        while (currentNode != null) {
+            System.out.println(currentNode.myValue);
+            currentNode = currentNode.nextNode;
+        }
+    }
+
+    public void printBackwards() {
+        MyDoubleLinkListNode currentNode = tail;
+        while (currentNode != null) {
+            System.out.println(currentNode.myValue);
+            currentNode = currentNode.previousNode;
+        }
+    }
 }
--- src/main/java/com/revature/nwarner/main/Tester.java
+++ src/main/java/com/revature/nwarner/main/Tester.java
@@ -1,6 +1,7 @@
 package com.revature.nwarner.main;
 
 import com.revature.nwarner.collections.MyArrayList;
+import com.revature.nwarner.collections.MyDoubleLinkList;
 
 public class Tester {
 
@@ -18,5 +19,18 @@ public class Tester {
             System.out.println(mal.getItem(x));
         }
 
+        MyDoubleLinkList mdll = new MyDoubleLinkList();
+
+        for(int x = 0; x < 5; x++) {
+            mdll.push(x);
+        }
+        for(int x = 100; x < 104; x++) {
+            mdll.add(x);
+        }
+        System.out.println("Forward");
+        mdll.printForward();
+
+        System.out.println("Backwards");
+        mdll.printBackwards();
     }
 }
